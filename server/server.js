180742670835e1731675035bde6bd78983e9f5c3
kendall baker <d00262562@dmail.dixie.edu>
2023-01-25 12:01:16 -0700

    adding node

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+
+
+// express app
+const app = express()
+
+//routes
+app.get('/', (req, response) => {
+    response.json({ mssg: 'Welcome to the app' })
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
