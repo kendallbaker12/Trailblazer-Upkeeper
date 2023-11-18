be9a6fa0df05e3ca3501ee15eb3f1f3d218ec6f8
Kendall <kendall.baker@tcnbroadcasting.com>
2023-11-17 17:34:57 -0700

    pushing to github

--- client/src/pages/Buildings.js
+++ client/src/pages/Buildings.js
@@ -1,52 +1,56 @@
-import { useEffect, useState } from "react"
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 //import { useBuildingContext } from "../hooks/useBuildingsContext"
 
 // components
-import BldDetails from "../components/bldDetails"
-import BuildingForm from "../components/BuildingForm"
-import RoomForm from "../components/roomForm"
-
-
+import BldDetails from "../components/bldDetails";
+import BuildingForm from "../components/BuildingForm";
+import RoomForm from "../components/roomForm";
 
 const Buildings = () => {
-    // const { buildings, dispatch } = useBuildingContext()
-    const [buildings, setBuildings] = useState(null)
+  // const { buildings, dispatch } = useBuildingContext()
+  const [buildings, setBuildings] = useState(null);
 
-    useEffect(() => {
-        const fetchBuildings = async () => {
-            const response = await fetch('https://trailblazerupkeeper.onrender.com/buildings')
-            const bldjson = await response.json()
+  useEffect(() => {
+    const fetchBuildings = async () => {
+      const response = await fetch(
+        "https://trailblazerupkeeper.onrender.com/buildings"
+      );
+      const bldjson = await response.json();
 
-            if (response.ok) {
-                // dispatch({ type: 'SET_BUILDINGS', payload: json })
-                setBuildings(bldjson)
+      if (response.ok) {
+        // dispatchcadding comment({ type: 'SET_BUILDINGS', payload: json })
+        setBuildings(bldjson);
+      }
+    };
+    fetchBuildings();
+  }, []);
 
-            }
-        }
-        fetchBuildings()
-
-    }, [])
-
-
-    return (
-        <div className="mt-16">
-            <div className="py-8 bg-gray-900">
-                <div className="container px-8 mx-auto">
-                    <img className="object-cover w-full h-64 lg:h-80" src="https://campus.utahtech.edu/wp-content/themes/dixie-state-university/assets/images/Open-Graph-copy.jpg" ></img>
-                </div>
-            </div>
-            <div className="container mx-auto mt-20">
-                <div className="grid grid-cols-1 gap-2 sm:grid-cols-1 lg:grid-cols-2 ">
-                    {buildings && buildings.map((building) => (
-                        <div key={building._id} className="bg-[#003058] rounded-lg shadow-md p-4 shadow-lg shadow-[#ba1c21]">
-                            <BldDetails buildings={building} />
-                        </div>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className="mt-16">
+      <div className="py-8 bg-gray-900">
+        <div className="container px-8 mx-auto">
+          <img
+            className="object-cover w-full h-64 lg:h-80"
+            src="https://campus.utahtech.edu/wp-content/themes/dixie-state-university/assets/images/Open-Graph-copy.jpg"
+          ></img>
         </div>
-    );
-}
+      </div>
+      <div className="container mx-auto mt-20">
+        <div className="grid grid-cols-1 gap-2 sm:grid-cols-1 lg:grid-cols-2 ">
+          {buildings &&
+            buildings.map((building) => (
+              <div
+                key={building._id}
+                className="bg-[#003058] rounded-lg shadow-md p-4 shadow-lg shadow-[#ba1c21]"
+              >
+                <BldDetails buildings={building} />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Buildings
+export default Buildings;
